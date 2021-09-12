a8de8431aeed8f8da949fb63a2c7b569000e41bb
Davide Lo Cascio <davide.atticode@gmail.com>
2021-09-12 11:53:04 +0200

    added projects files

---
+++ Scripts/Script.sql
@@ -0,0 +1,4 @@
+DROP SCHEMA IF EXISTS vbzdat;
+-- In MySQL, the schema is the synonym for the database -- DROP DATABASE [IF EXISTS] database_name;
+CREATE SCHEMA vbzdat DEFAULT CHARACTER SET utf8; 
+USE vbzdat;
